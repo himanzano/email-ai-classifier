c230509d6ae58890a257838f265497740aea1c7f
Marlon Couto <ozaircouto@outlook.com>
2026-01-16 12:42:40 -0300

    feat: load environment variables from .env file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from typing import Union
 
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from pathlib import Path
+
+# Resolver o path garante previsibilidade independente do local de execução.
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_PATH = BASE_DIR / ".env"
+
+load_dotenv(dotenv_path=ENV_PATH)  # Carrega variáveis de ambiente do arquivo .env
 
 app = FastAPI()
 
